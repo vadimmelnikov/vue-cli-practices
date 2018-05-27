3cb99f578227c5eb1b3ab63ef1fad3826a241c0e
Vadim Melnikov <Vadimvadic_m@mail.ru>
2018-05-27 03:26:00 +0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+    .hello
+        h1 {{ msg }}
+        p
+            | For guide and recipes on how to configure / customize this project,
+            br
+            | check out the
+            |
+            a(href="https://github.com/vuejs/vue-cli/tree/dev/docs", target="_blank") vue-cli documentation
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="box">
+        <p>
+            <slot></slot>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "message"
+    }
+</script>
+
+<style scoped>
+    .box{
+        color: aquamarine;
+    }
+
+</style>
